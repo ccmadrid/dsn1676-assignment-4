74fde5673d8025a26fdd35639e2cc6836f6af5d0
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2015-02-12 11:19:51 -0500

    created objects

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var blueberries = {
+	name: 'Blueberries',
+	species: 'Vaccinium corymbosum',
+	growsOnShrub: true,
+	energy: '240 kJ',
+	carbohydrates: '14.49 g',
+	protein: '0.74 g'
+};
+
+var grapes = {
+	name: 'Grapes',
+	species: 'Vitis vinifera',
+	growsOnShrub: false,
+	energy: '288 kJ',
+	carbohydrates: '18.1 g',
+	protein: '0.72 g'
+};
+var RedCurrant = {
+	name: 'Red currant',
+	species: 'Ribes rubrum',
+	growsOnShrub: true,
+	energy: '234 kJ',
+	carbohydrates: '13.8 g',
+	protein: '1.4 g'
+};
